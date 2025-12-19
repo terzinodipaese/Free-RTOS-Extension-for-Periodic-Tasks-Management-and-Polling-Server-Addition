3285f624a0ace4ad651d795065467c36ecfaf065
mrdega <davide.garrone.dg@gmail.com>
2025-12-19 11:35:27 +0100

    feat: added logger.c and logger.h

---
+++ FreeRTOS/FreeRTOS/Source/include/logger.h
@@ -0,0 +1,23 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include "FreeRTOS.h"
+
+#define LOGGER_BUFFER_SIZE 1024
+
+typedef enum {
+    LOGGER_TASK_RELEASE,
+    LOGGER_TASK_START,
+    LOGGER_TASK_END,
+    LOGGER_TASK_DEADLINE_MISS,
+    LOGGER_IDLE
+} LoggerEventType_t;
+
+
+typedef struct {
+    const char* pcTaskName;
+    TickType_t ulTimestamp;
+    LoggerEventType_t eEventType;
+} LoggerEntry_t;
+
+#endif
